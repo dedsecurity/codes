a1029380c58a51d3d35291a784b1d23727461108
simdok <kinjosimon@gmail.com>
2022-02-10 14:54:43 -0300

    codes

--- main.py
+++ main.py
@@ -57,6 +57,51 @@ else:
 print("REPLICAS: {}".format(strategy.num_replicas_in_sync))
 """
 
+class Nl2PyTranslator(tf.keras.Model):
+    def __init__(self, nl_text_processor, py_text_process, fixed_embedding, unit=128):
+        super().__init__()
+        # Natural language
+        self.nl_text_processor = nl_text_processor
+        self.nl_voba_size = len(nl_text_processor.get_vocabulary())
+        self.nl_embedding = tf.keras.layers.Embedding(
+            self.nl_voba_size,
+            output_dim=unit,
+            mask_zero=True)
+        self.fixed_embedding = fixed_embedding
+        self.nl_rnn = tf.keras.layers.Bidirectional(layer=tf.keras.layers.LSTM(int(unit/2), return_sequences=True, return_state=True))
+        # Attention
+        self.attention = tf.keras.layers.Attention()
+        # PY
+        self.py_text_process = py_text_process
+        self.py_voba_size = len(py_text_process.get_vocabulary())
+        self.py_embedding = tf.keras.layers.Embedding(
+            self.py_voba_size,
+            output_dim=unit,
+            mask_zero=True)
+        self.py_rnn = tf.keras.layers.LSTM(unit, return_sequences=True, return_state=True)
+        # Output
+        self.out = tf.keras.layers.Dense(self.py_voba_size)
+            
+    def call(self, nl_text, py_text, training=True):
+        nl_tokens = self.nl_text_processor(nl_text) # Shape: (batch, Ts)
+        nl_vectors = self.nl_embedding(nl_tokens, training=training) # Shape: (batch, Ts, embedding_dim)
+        nl_fixed_vectors = self.fixed_embedding(nl_tokens) # Shape: (batch, Ts, 100)
+        nl_combined_vectors = tf.concat([nl_vectors, nl_fixed_vectors], -1) # Shape: (batch, Ts, embedding_dim+100)
+        nl_rnn_out, fhstate, fcstate, bhstate, bcstate = self.nl_rnn(nl_vectors, training=training) # Shape: (batch, Ts, bi_rnn_output_dim), (batch, rnn_output_dim) ...
+        nl_hstate = tf.concat([fhstate, bhstate], -1)
+        nl_cstate = tf.concat([fcstate, bcstate], -1)
+        
+        py_tokens = self.py_text_process(py_text) # Shape: (batch, Te)
+        expected = py_tokens[:,1:] # Shape: (batch, Te-1)
+        
+        teacher_forcing = py_tokens[:,:-1] # Shape: (batch, Te-1)
+        py_vectors = self.py_embedding(teacher_forcing, training=training) # Shape: (batch, Te-1, embedding_dim)
+        py_in = self.attention(inputs=[py_vectors,nl_rnn_out], mask=[py_vectors._keras_mask, nl_rnn_out._keras_mask], training=training)
+        
+        trans_vectors, _, _ = self.py_rnn(py_in, initial_state=[nl_hstate, nl_cstate], training=training) # Shape: (batch, Te-1, rnn_output_dim)
+        out = self.out(trans_vectors, training=training) # Shape: (batch, Te-1, py_vocab_size)
+        return out, expected, out._keras_mask
+
 with open('./content.json') as content:
   databa = json.load(content)
 
